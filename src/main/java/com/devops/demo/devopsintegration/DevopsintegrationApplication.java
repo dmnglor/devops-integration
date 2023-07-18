88287d356bbd6b062fadfa22417d5a69882c4a07
dmnglore <35909263+dmnglor@users.noreply.github.com>
2023-07-18 15:30:50 +0530

    first commit

---
+++ src/main/java/com/devops/demo/devopsintegration/DevopsintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.devops.demo.devopsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsintegrationApplication.class, args);
+	}
+
+}
